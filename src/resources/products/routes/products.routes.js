21c1b1ad5ed8e58126dae36eba3c95a2fa20765b
Jana <janagonzalez.f@gmail.com>
2023-07-17 20:52:21 -0400

    url prod

--- src/resources/products/routes/products.routes.js
+++ src/resources/products/routes/products.routes.js
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import { isAdmin, verifyToken } from '../../auth/middlewares/auth.middleware.js'
 import { createProduct, getProductById, getProducts } from '../controllers/products.controller.js'
 
 const productsRouter = Router()
 const baseURI = '/products'
 
 productsRouter .post( baseURI, createProduct )
-productsRouter .get( baseURI, verifyToken, getProducts )
-productsRouter .get( `${ baseURI }/:id`, verifyToken, getProductById )
+productsRouter .get( baseURI, getProducts )
+productsRouter .get( `${ baseURI }/:id`, getProductById )
 
 export default productsRouter
